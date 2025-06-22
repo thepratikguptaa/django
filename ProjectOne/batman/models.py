f14fde2e05ec1f1963486756381154b7cc87f2be
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-23 00:18:36 +0530

    Add description field to SaveGotham model and update views and templates for display

--- ProjectOne/batman/models.py
+++ ProjectOne/batman/models.py
@@ -14,6 +14,7 @@ class SaveGotham(models.Model):
     image = models.ImageField(upload_to="save-gotham/")
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=WEAPON_CHOICE)
+    description = models.TextField(default="No description provided.")
 
     def __str__(self):
         return self.name # return the name of the save
--- ProjectOne/batman/views.py
+++ ProjectOne/batman/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import SaveGotham
 
 # Create your views here.
 def batman(request):
-    return render(request, 'batman/im_batman.html')
+    batmen = SaveGotham.objects.all()
+    return render(request, 'batman/im_batman.html', {'batmen': batmen})
