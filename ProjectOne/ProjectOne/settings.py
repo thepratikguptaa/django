fd9a38a481aba3a91812dc146965aa0766291644
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-22 17:52:02 +0530

    Add Tailwind CSS integration with PostCSS setup
    - Created package.json for managing dependencies and scripts for building and developing styles.
    - Added postcss.config.js to configure PostCSS with Tailwind CSS, postcss-simple-vars, and postcss-nested plugins.
    - Introduced styles.css to import Tailwind CSS and define the source paths for scanning templates and scripts.
    - Created base.html template to demonstrate the integration of Django with Tailwind CSS, including necessary meta tags and a sample layout.

--- ProjectOne/ProjectOne/settings.py
+++ ProjectOne/ProjectOne/settings.py
@@ -38,7 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'batman',
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1'] # for local development
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
