c03790f1886940e59c5e02139c974447bd40eac6
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-23 14:20:24 +0530

    Add CandidateReviews model and enhance image display in candidate list

--- ProjectOne/batman/models.py
+++ ProjectOne/batman/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -18,3 +19,22 @@ class SaveGotham(models.Model):
 
     def __str__(self):
         return self.name # return the name of the save
+    
+
+#one to many relationship model
+class CandidateReviews(models.Model):
+    RATING_OPTIONS = [
+        ("1/5", "1/5: Not Worthy"),
+        ("2/5", "2/5: Poor"),
+        ("3/5", "3/5: Average"),
+        ("4/5", "4/5: Good"),
+        ("5/5", "5/5: Best"),
+    ]
+    candidate = models.ForeignKey(SaveGotham, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.CharField(max_length=100, choices=RATING_OPTIONS)
+    comment = models.TextField(default="No comment provided.")
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.candidate.name}'
